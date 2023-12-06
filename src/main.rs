ef76b048fd2389dc82752eca0053d67e2618be57
Jean Carlos Taveras <jean@movatic.co>
2023-12-06 10:16:59 -0400

    Use lines instead of split

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,15 @@ const BAG: [(&'static str, i32); 3] = [("blue", 14), ("green", 13), ("red", 12)]
 
 fn main() {
     let content = fs::read_to_string("./input.txt").expect("File should exit.");
-    let result: i32 = content
-        .split('\n')
-        .collect::<Vec<&str>>()
-        .iter()
-        .map(get_valid_game_id)
-        .sum();
+    let result: i32 = content.lines().map(get_valid_game_id).sum();
+    let power: i32 = content.lines().map(get_power_values).sum();
 
     println!("Sum of all game ID is: {result}");
-
-    let power: i32 = content
-        .split('\n')
-        .collect::<Vec<&str>>()
-        .iter()
-        .map(get_power_values)
-        .sum();
-    
     println!("Sum of the power is: {power}");
 }
 
-fn get_valid_game_id(value: &&str) -> i32 {
-    let game_info = value.split(':').collect::<Vec<&str>>();
+fn get_valid_game_id(line_value: &str) -> i32 {
+    let game_info = line_value.split(':').collect::<Vec<&str>>();
     let games = game_info[1]
         .split(';')
         .collect::<Vec<&str>>()
@@ -69,22 +57,21 @@ fn check_game(hints: &Vec<&str>) -> bool {
     result
 }
 
-fn get_power_values(value: &&str) -> i32 {
+fn get_power_values(value: &str) -> i32 {
     let game_info = value.split(':').collect::<Vec<&str>>();
     let games = game_info[1]
         .split(';')
         .collect::<Vec<&str>>()
         .iter()
         .map(|x| x.trim().split(", ").collect::<Vec<&str>>())
-        .collect::<Vec<Vec<&str>>>(); 
+        .collect::<Vec<Vec<&str>>>();
 
     let mut minimum_game_value: [(&str, i32); 3] = [
         ("blue", std::i32::MIN),
         ("green", std::i32::MIN),
-        ("red", std::i32::MIN)
+        ("red", std::i32::MIN),
     ];
     let mut index = 0;
-
 
     for game in games {
         for round in game {
@@ -95,7 +82,10 @@ fn get_power_values(value: &&str) -> i32 {
                     .expect("Amount should be a valid number");
 
                 while index < minimum_game_value.len() {
-                    if minimum_game_value[index].0 == color && minimum_game_value[index].1 < amount {
+                    let should_update_min_value = minimum_game_value[index].0 == color
+                        && minimum_game_value[index].1 < amount;
+
+                    if should_update_min_value {
                         minimum_game_value[index].1 = amount;
                     }
 
@@ -109,5 +99,9 @@ fn get_power_values(value: &&str) -> i32 {
         }
     }
 
-    minimum_game_value.iter().map(|x| x.1).reduce(|acc, current| acc * current).expect("Should have valid values")
+    minimum_game_value
+        .iter()
+        .map(|x| x.1)
+        .reduce(|acc, current| acc * current)
+        .expect("Should have valid values")
 }
